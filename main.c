7bc03e6cb1c3cb24d38c4bebbdc662c9d192a822
Marc <RhinoDevel@users.noreply.github.com>
2016-09-01 23:03:15 +0200

    Initial debug player control and debug "game" loop.

--- main.c
+++ main.c
@@ -9,12 +9,13 @@
 #include "Mt3d.h"
 #include "MapSample.h"
 #include "Bmp.h"
+#include "Sys.h"
 
-static int const WIDTH = 300;
+static int const WIDTH = 320;
 static int const HEIGHT = 200;
-static int const ALPHA = 90;
-static int const BETA = 60;
-static double const H = 0.5;
+static int const ALPHA = 64;
+static int const BETA = 40;
+static double const H = 0.3;
 
 int main()
 {
@@ -26,22 +27,59 @@ int main()
     o->posX = o->map->posX;
     o->posY = o->map->posY;
     o->gamma = o->map->gamma;
+
+    for(int row = 0, col = 0;row<HEIGHT;++row)
+    {
+        unsigned char * const rowPtr = o->pixels+row*WIDTH*3; 
+
+        for(col = 0;col<3*WIDTH;col+=3)
+        {
+            rowPtr[col+0] = 0xFF; // Blue
+            rowPtr[col+1] = 0x00; // Green
+            rowPtr[col+2] = 0xFF; // Red
+        }
+    }
     
-//    {
-//        for(int row = 0, col = 0;row<HEIGHT;++row)
-//        {
-//            unsigned char * const rowPtr = o->pixels+row*WIDTH*3; 
-//
-//            for(col = 0;col<3*WIDTH;col+=3)
-//            {
-//                rowPtr[col+0] = 0x00; // Blue
-//                rowPtr[col+1] = 0x00; // Green
-//                rowPtr[col+2] = 0xFF; // Red
-//            }
-//        }
-//    }	
+    bool exit = false;
+    do
+    {
+        Mt3d_draw(o);
     
-    Bmp_write(WIDTH, HEIGHT, o->pixels, "out.bmp");
+        int const playerX = (int)o->posX,
+            playerY = (int)o->posY;
+        Map_print(o->map, &playerX, &playerY);
+        Bmp_write(WIDTH, HEIGHT, o->pixels, "out.bmp");
+        
+        char * const input = Sys_get_stdin();
+        switch(input[0])
+        {
+            case 'a':
+                o->gamma = o->gamma+23.0;
+                break;
+            case 's':
+                break;
+            case 'd':
+                o->gamma = o->gamma-23.0;
+                break;
+            case 'w':
+                break;
+            
+            default:
+                exit = true;
+                break;
+        }
+        if(o->gamma<0.0)
+        {
+            o->gamma = 360.0+o->gamma;
+        }
+        else
+        {
+            if(o->gamma>=360.0)
+            {
+                o->gamma -= 360.0;
+            }
+        }
+    }while(!exit);
     
     Map_delete(o->map);
     o->map = NULL;
